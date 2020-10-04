b3b309305187dc76eee69e00131eb7867b672ec9
Karine Samyn <Karine.Samyn@hogent.be>
2020-10-04 19:06:39 +0200

    Add project files.

---
+++ BlackJackGame/Models/Hand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BlackJackGame.Models
+{
+    public class Hand
+    {
+        #region Fields
+        private readonly IList<BlackJackCard> _cards;
+        #endregion
+
+        #region Properties
+        public IEnumerable<BlackJackCard> Cards { get { throw new NotImplementedException(); } }
+
+        public int NrOfCards { get { throw new NotImplementedException(); } }
+
+        public int Value { get { throw new NotImplementedException(); } }
+        #endregion
+
+        #region Constructors
+        public Hand()
+        {
+            _cards = new List<BlackJackCard>();
+        }
+        #endregion
+
+        #region Methods
+        public void AddCard(BlackJackCard blackJackCard)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TurnAllCardsFaceUp()
+        {
+            throw new NotImplementedException();
+        }
+        private int CalculateValue()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
